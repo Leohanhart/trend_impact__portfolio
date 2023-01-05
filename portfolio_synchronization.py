e6e1f025c61d74d3d732e366d3accfef50a7ec58
Leohanhart <lhanhart1996@gmail.com>
2023-01-05 10:21:59 +0100

    Create portfolio_synchronization.py
    - Used to update, delete, expire portfolio's

---
+++ portfolio_synchronization.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 14:53:01 2023
+
+@author: Gebruiker
+
+all portfolio's have certain experation criteria, these need to be managed in this file']
+
+so - load all portfolio's check the options and do the actions.'
+
+"""
