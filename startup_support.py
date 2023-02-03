4acd25d29791edc1307cc032a131e5ae188255d9
Leohanhart <lhanhart1996@gmail.com>
2023-02-03 17:17:34 +0100

    Added startup sopport.
    - added startup sopport
    - added function to check if its the first of the month.
    - Should add if day is business day.

---
+++ startup_support.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 11:34:06 2023
+
+@author: Gebruiker
+"""
+from datetime import datetime, timedelta
+import pandas as pd
+from pandas.tseries.holiday import USFederalHolidayCalendar
+import numpy as np
+
+
+def check_if_today_is_first_the_month():
+    """
+    Check if today is first business day of the month. 
+
+    Returns
+    -------
+    bool
+        DESCRIPTION.
+
+    """
+
+    dt = datetime.now()
+    td = timedelta(days=5)
+    # your calculated date
+    my_date = dt + td
+
+    first_business_days = pd.date_range(dt.strftime(
+        "%d-%m-%Y"), my_date.strftime("%d-%m-%Y"),
+    )  # freq='BMS')
+
+    if np.datetime64(dt, 'D') in first_business_days:
+        return True
+    else:
+        return False
+
+
+def check_if_today_is_first_the_quarter():
+    """
+
+    Check if today is first business day of the quarter. 
+
+    Returns
+    -------
+    bool
+        DESCRIPTION.
+
+    """
+
+    dt = datetime.now()
+    td = timedelta(days=365)
+    # your calculated date
+    my_date = dt + td
+
+    first_business_days = pd.date_range(dt.strftime(
+        "%d-%m-%Y"), my_date.strftime("%d-%m-%Y"), freq='BMS')
+
+    if np.datetime64(dt, 'D') in first_business_days:
+        return True
+    else:
+        return False
