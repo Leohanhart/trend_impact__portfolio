0b6f7bcd067f5ff955c366f0c0fd67243b330834
Leohanhart <lhanhart1996@gmail.com>
2023-02-05 17:08:01 +0100

    added new libs and poort

--- main.py
+++ main.py
@@ -113,5 +113,5 @@ handler = Mangum(app)
 
 if __name__ == "__main__":
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, debug=True,
+    uvicorn.run("main:app", host="0.0.0.0", port=8005, reload=True, debug=True,
                 workers=4, limit_concurrency=1000)
