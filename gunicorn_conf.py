1f84de9efd150a3d44ceb3c33970c71001ed6362
Leohanhart <lhanhart1996@gmail.com>
2023-04-11 16:38:59 +0200

    update guicore

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,13 +1,9 @@
+# gunicorn_conf.py
 from multiprocessing import cpu_count
 
-# Socket Path
-bind = 'unix:/home/flowimpact/flowimpact_core/gunicorn.sock'
+bind = "127.0.0.1:8000"
 
 # Worker Options
 workers = cpu_count() + 1
 worker_class = 'uvicorn.workers.UvicornWorker'
 
-# Logging Options
-loglevel = 'debug'
-accesslog = '/home/flowimpact/flowimpact_core/access_log'
-errorlog =  '/home/flowimpact/flowimpact_core/error_log'
